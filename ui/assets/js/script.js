f61d4a40755e348c6298027eb8d1133b86ebeb76
Desmond Edem <edemintech@gmail.com>
2019-04-23 12:03:37 +0100

    fix(homepage): add responsiveness script

---
+++ ui/assets/js/script.js
@@ -0,0 +1,10 @@
+/**
+ * Event handler for toggling menu on mobile devices
+ */
+
+const hamburgerBtn = document.querySelector('.hamburger-menu');
+hamburgerBtn.addEventListener('click', function toggleMenu() {
+  const menu = document.querySelector('.navbar-menu');
+  this.classList.toggle('menu-open');
+  menu.classList.toggle('menu-open');
+});
