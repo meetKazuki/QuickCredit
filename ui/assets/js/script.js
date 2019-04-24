a4a4a77ad09e079051f2b18f04d365e404500d85
Desmond Edem <edemintech@gmail.com>
2019-04-24 14:05:04 +0100

    feat(admin): view all repaid loans
    [Start #165332061]

--- ui/assets/js/script.js
+++ ui/assets/js/script.js
@@ -1,9 +1,22 @@
 /**
  * Event handler for toggling menu on mobile devices
  */
-const hamburgerBtn = document.querySelector('.hamburger-menu');
-hamburgerBtn.addEventListener('click', function toggleMenu() {
-  const menu = document.querySelector('.navbar-menu');
-  this.classList.toggle('menu-open');
-  menu.classList.toggle('menu-open');
-});
+// const hamburgerBtn = document.querySelector('.hamburger-menu');
+// hamburgerBtn.addEventListener('click', function toggleMenu() {
+//   const menu = document.querySelector('.navbar-menu');
+//   this.classList.toggle('menu-open');
+//   menu.classList.toggle('menu-open');
+// });
+
+/**
+ * Event handler for displaying table according to filter
+ */
+const openTab = (tabName, getBtn) => {
+  document.querySelectorAll(".responsive-table").forEach((element) => {
+    element.style.display = "none";
+  });
+
+  document.querySelector(".btn-toggled").classList.remove("btn-toggled");
+  getBtn.classList.add("btn-toggled");
+  document.querySelector(`#${tabName}`).style.display = "block";
+};
