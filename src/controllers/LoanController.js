5d89b599bb6ba56ab3040b7481429637d196c2e4
Desmond Edem <edemintech@gmail.com>
2019-05-06 12:05:04 +0100

    feat(loan): user can apply for a loan
    [Start #165772278]

--- src/controllers/LoanController.js
+++ src/controllers/LoanController.js
@@ -7,6 +7,34 @@ import Loan from '../models/Loan';
  */
 class LoanController {
   /**
+   * @method createLoan
+   * @description Creates a loan application request
+   * @param {object} req - The Request Object
+   * @param {object} res - The Response Object
+   * @returns {object} JSON API Response
+   */
+  static createLoan(req, res) {
+    const newLoan = Loan.create(req.body);
+
+    res.status(201).json({
+      status: 201,
+      data: {
+        message: 'Loan request received. We\'ll get back to you shortly.',
+        loanId: newLoan.id,
+        firstName: req.body.firstName,
+        lastName: req.body.lastName,
+        email: newLoan.user,
+        tenor: newLoan.tenor,
+        amount: newLoan.amount,
+        monthlyInstallment: newLoan.paymentInstallment,
+        status: newLoan.status,
+        balance: newLoan.balance,
+        interest: newLoan.interest,
+      },
+    });
+  }
+
+  /**
    * @method getAllLoans
    * @description List all loan applications in the database
    * @param {object} req - The Request Object
